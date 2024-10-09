b3f5faa71bcbe108ccba8dd272dd7cc07a272785
SweelLok <94239927+SweelLok@users.noreply.github.com>
2024-10-09 20:44:57 +0300

    Update main.py

--- second_task/SEARCH WAY/main.py
+++ second_task/SEARCH WAY/main.py
@@ -1,11 +1,1 @@
-#Імпорти
-import customtkinter as ctk
 
-from labyrinth import labyrinth
-from LabApp import LabyrinthApp
-
-
-# Запуск
-if __name__ == "main":
-    app = LabyrinthApp()
-    app.mainloop()
